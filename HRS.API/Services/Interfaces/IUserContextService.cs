66ec8a2e4badcabe911f274c10700f386e42d81b
Feri Setiawan <e1546606@u.nus.edu>
2025-09-28 01:16:08 +0800

    [HRS-55] Enable Active User to API

--- HRS.API/Services/Interfaces/IUserContextService.cs
+++ HRS.API/Services/Interfaces/IUserContextService.cs
@@ -1,3 +1,4 @@
+using HRS.API.Contracts.DTOs.User;
 using HRS.Domain.Entities;
 
 namespace HRS.API.Services.Interfaces;
@@ -5,6 +6,8 @@ namespace HRS.API.Services.Interfaces;
 public interface IUserContextService
 {
     Task<User> GetUserAsync();
+
+    Task<UserDto> GetUserDtoAsync();
 
     Task<int> GetUserIdAsync();
 }
