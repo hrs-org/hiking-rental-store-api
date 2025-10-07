abebe0b19719a8a32315baceb6d7958787e67c94
EvanQwQ-CMD <e1554482@u.nus.edu>
2025-10-07 18:14:57 +0800

    [HRS-45]Forget-Password-Api

---
+++ HRS.API/Contracts/DTOs/Auth/ForgotPasswordResponseDto.cs
@@ -0,0 +1,7 @@
+namespace HRS.API.Contracts.DTOs.Auth;
+
+public class ForgotPasswordResponseDto
+{
+    public bool IsSuccess { get; set; }
+    public string? Message { get; set; }
+}
---
+++ HRS.API/Contracts/DTOs/Auth/ResetPasswordResponseDto.cs
@@ -0,0 +1,7 @@
+namespace HRS.API.Contracts.DTOs.Auth;
+
+public class ResetPasswordResponseDto
+{
+    public bool IsSuccess { get; set; }
+    public string? Message { get; set; }
+}
